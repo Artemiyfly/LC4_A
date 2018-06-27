44adac222cc0f9b02729b161b602cd9e82bcfe33
Artemiyfly <Artemiy.fly27@gmail.com>
2018-06-27 14:27:58 +0300

    using fix and class move

--- A.cpp
+++ A.cpp
@@ -3,7 +3,10 @@
 #include <memory>
 #include <queue>
 
-using namespace std;
+using std::vector;
+using std::shared_ptr;
+using std::priority_queue;
+using std::pair;
 
 const int64_t INF = 1e10;
 
@@ -14,72 +17,73 @@ vector<T> &operator+=(vector <T> &a, const vector <T> &b) {
     return a;
 }
 
+class Iedge {
+ public:
+    virtual int id() = 0;
+    virtual int from() = 0;
+    virtual int to() = 0;
+    virtual int flow() = 0;
+    virtual int price() = 0;
+    virtual int free_cap() = 0;
+    virtual void pass_flow(int df) = 0;
+};
+class mirror_edge;
+class edge: public Iedge {
+    int pid, pfrom, pto, pcap, pflow, pprice;
+public:
+    edge(int id, int from, int to, int cap, int price):
+        pid(id), pfrom(from), pto(to), pcap(cap), pflow(0), pprice(price) {}
+    int id() {
+        return pid;
+    }
+    int from() {
+        return pfrom;
+    }
+    int to() {
+        return pto;
+    }
+    int flow() {
+        return pflow;
+    }
+    int price() {
+        return pflow >= 0 ? pprice : -pprice;
+    }
+    int free_cap() {
+        return pflow >= 0 ? pcap - pflow : -pflow;
+    }
+    void pass_flow(int df) {
+        pflow += df;
+    }
+    friend class mirror_edge;
+};
+class mirror_edge: public Iedge {
+    shared_ptr <edge> real;
+public:
+    mirror_edge(shared_ptr <edge> real): real(real) {}
+    int id() {
+        return real->pid;
+    }
+    int from() {
+        return real->pto;
+    }
+    int to() {
+        return real->pfrom;
+    }
+    int flow() {
+        return -real->pflow;
+    }
+    int price() {
+        return real->pflow <= 0 ? real->pprice : -real->pprice;
+    }
+    int free_cap() {
+        return real->pflow <= 0 ? real->pflow + real->pcap : real->pflow;
+    }
+    void pass_flow(int df) {
+        real->pass_flow(-df);
+    }
+};
+
 class flow_graph {
-    class Iedge {
-     public:
-        virtual int id() = 0;
-        virtual int from() = 0;
-        virtual int to() = 0;
-        virtual int flow() = 0;
-        virtual int price() = 0;
-        virtual int free_cap() = 0;
-        virtual void pass_flow(int df) = 0;
-    };
-    class mirror_edge;
-    class edge: public Iedge {
-        int pid, pfrom, pto, pcap, pflow, pprice;
-    public:
-        edge(int id, int from, int to, int cap, int price):
-            pid(id), pfrom(from), pto(to), pcap(cap), pflow(0), pprice(price) {}
-        int id() {
-            return pid;
-        }
-        int from() {
-            return pfrom;
-        }
-        int to() {
-            return pto;
-        }
-        int flow() {
-            return pflow;
-        }
-        int price() {
-            return pflow >= 0 ? pprice : -pprice;
-        }
-        int free_cap() {
-            return pflow >= 0 ? pcap - pflow : -pflow;
-        }
-        void pass_flow(int df) {
-            pflow += df;
-        }
-        friend class mirror_edge;
-    };
-    class mirror_edge: public Iedge {
-        shared_ptr <edge> real;
-    public:
-        mirror_edge(shared_ptr <edge> real): real(real) {}
-        int id() {
-            return real->pid;
-        }
-        int from() {
-            return real->pto;
-        }
-        int to() {
-            return real->pfrom;
-        }
-        int flow() {
-            return -real->pflow;
-        }
-        int price() {
-            return real->pflow <= 0 ? real->pprice : -real->pprice;
-        }
-        int free_cap() {
-            return real->pflow <= 0 ? real->pflow + real->pcap : real->pflow;
-        }
-        void pass_flow(int df) {
-            real->pass_flow(-df);
-        }
-    };
     vector <int64_t> Dijkstra() {
         vector <int64_t> d(h.size(), INF);
         priority_queue <pair<int64_t, int> > q;
@@ -175,6 +179,7 @@ public:
 };
 
 void read(flow_graph &g, int m) {
+    using std::cin;
     for (int i = 0; i < m; ++i) {
         int cfrom, cto, cw;
         cin >> cfrom >> cto >> cw;
@@ -183,6 +188,9 @@ void read(flow_graph &g, int m) {
 }
 
 void print(flow_graph &g, int k) {
+    using std::cout;
+    using std::fixed;
+    using std::endl;
     cout.precision(8);
     cout << fixed << g.get_cost() * 1.0 / k << endl;
     for (int i = 0; i < k; ++i) {
@@ -195,6 +203,8 @@ void print(flow_graph &g, int k) {
 }
 
 int main() {
+    using std::cin;
+    using std::cout;
     int n, m, k;
     cin >> n >> m >> k;
     flow_graph g(n);
